910c1f4b1f35ebda384eb05c66200091a22394d9
ValeeBravo <163045037+ValeeBravo@users.noreply.github.com>
2024-03-25 17:37:44 -0300

    Add files via upload

--- estudiantes/ValeeBravo/clase-03/sketch.js
+++ estudiantes/ValeeBravo/clase-03/sketch.js
@@ -1,29 +1,26 @@
-function setup() {
-  createCanvas(400, 400);
+let fotito;
 
+
+function preload() {
+ fotito = loadImage("./yor.jpg");
+}
+  
+function setup() {
+  createCanvas(800, 800);
 }
 
 function draw() {
-  background(220); 
-  // sacado de color background() https://p5js.org/reference/#/p5/background
- background('rgba(0, 255, 0, 0.25)');
-  pintarRectanguloVerde(random(width),random(height));
-  
-  noStroke()
-  rect(width/2, height/2, 10, 200)
-  fill(204, 300, 0);
-  noStroke();
-  //elipse color amarillo en movimiento del mouse
-  ellipse(100, mouseX, mouseY);
-  //elipse color amarillo estatico 
-ellipse(200, 200, 200, 200);  
-}
-
-function pintarVerde() {
-   noStroke();
-  fill(0, 180, 200)
-}
-function pintarRectanguloVerde(x, y) {
-  pintarVerde();
-  rect(x, y, 200, 100);  
-  }
+  background(220);
+  image(fotito, 0, 0, 300, 200);
+  //filter(POSTERIZE, 2);
+  loadPixels();
+  // console.log(pixels.length);
+  for(let i = 0; i < pixels.lenght; i++) {
+  pixels[index] = 0;
+      // Green.
+      pixels[index + 1] = 0;
+      pixels[index + 2] = 0;
+      pixels[index + 3] = 255;
+    }
+  updatePixels();
+ }
