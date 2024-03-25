3e37aded445aa42625211a1a1f6efb708d2ed2db
Camilarg7 <163043831+Camilarg7@users.noreply.github.com>
2024-03-25 17:34:48 -0300

    Add files via upload

---
+++ estudiantes/Camilarg7/clase-03/sketch.js
@@ -0,0 +1,29 @@
+//intento de funciones vistas en p5.js.org reference, para poder usar condicionales, al hacer click deberia cambiar el color del elipse
+
+
+function setup() {
+  createCanvas(700, 700);
+}
+
+function draw() {
+  background(100, 100, 240);
+  ellipse(mouseX, mouseY, 200, 200);
+  
+  fill(255, 0, 0);
+  noStroke();
+}
+{
+  mouseClicked(randomColor);
+}
+{
+  describe('A red ellipse changes color when the user releases a mouse press.')
+}
+function 
+  randomColor() {
+  let c = random(['red', 'yellow', 'blue', 'green']);
+  background(c);
+}
+if (click) {
+  console.log('cambio color');
+  
+}
