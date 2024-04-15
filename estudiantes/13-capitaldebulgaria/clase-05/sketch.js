07c748031b6e0f296eafc901a97f5c6f306ced1e
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-04-14 23:44:36 -0400

    corregir formato de README.md y enlaces a imagenes en sketch.js

--- estudiantes/13-capitaldebulgaria/clase-05/sketch.js
+++ estudiantes/13-capitaldebulgaria/clase-05/sketch.js
@@ -2,13 +2,14 @@
 //imágenes extraidas de google
 
 let x = 1,
-    y = 1;
-    
-function preload() { //presubir las imágenes
-  img = loadImage('/fondito.jpg'); //imagen de fondo como archivo
-  let img2 = loadImage('/cat.png'); //fotitos de gatos en formato png
-  let img3 = loadImage('/cat4.png');
-  imgs = [img2, img3,] //"carpetita" para seleccionar la cantidad de imágenes en específico (2)
+  y = 1;
+
+function preload() {
+  //presubir las imágenes
+  img = loadImage('./fondito.jpg'); //imagen de fondo como archivo
+  let img2 = loadImage('./cat.png'); //fotitos de gatos en formato png
+  let img3 = loadImage('./cat4.png');
+  imgs = [img2, img3]; //"carpetita" para seleccionar la cantidad de imágenes en específico (2)
 }
 
 function setup() {
@@ -17,18 +18,18 @@ function setup() {
 }
 
 function draw() {
-mouseClicked(); //función de click
-  randomImg = random(imgs) //que aparezcan gatitos random
+  mouseClicked(); //función de click
+  randomImg = random(imgs); //que aparezcan gatitos random
 }
 
-function drawGatitos(x,y){ //si hago click con el mouse aparece un gatito random
-    image(randomImg, pmouseX, pmouseY, 100, 100); //que los gatitos sigan el mouse y su tamaño
- } 
+function drawGatitos(x, y) {
+  //si hago click con el mouse aparece un gatito random
+  image(randomImg, pmouseX, pmouseY, 100, 100); //que los gatitos sigan el mouse y su tamaño
+}
 
-function mouseClicked(){
-
- if(mouseIsPressed){ 
-     drawGatitos(true,true);   
- }
-}  
-  //fin!!   
+function mouseClicked() {
+  if (mouseIsPressed) {
+    drawGatitos(true, true);
+  }
+}
+//fin!!
