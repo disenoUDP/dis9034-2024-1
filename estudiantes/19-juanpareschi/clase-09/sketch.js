58762b514f18da97e8feb335dd5316864d8bfd62
juan <163045421+juanpareschi@users.noreply.github.com>
2024-05-13 17:26:31 -0400

    Add files via upload

---
+++ estudiantes/19-juanpareschi/clase-09/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+
+
+/// NOTAS Y REFERENCIAS
+// https://johnprovencher.com/
+// https://www.youtube.com/watch?v=m_1lBVxIdM4
+// https://www.instagram.com/tim_rodenbroeker/
+// https://editor.p5js.org/codingtrain/sketches/KTVfEcpWx
+// https://www.tetoki.eu/asciiart/
+// https://editor.p5js.org/codingtrain/sketches/LNBpdYQHP
+// https://www.youtube.com/shorts/lHl_JTml_PA
+// https://dev.to/andyhaskell/convert-images-to-mosaics-in-p5js-2dlc
+// https://res.cloudinary.com/unit-london/video/upload/v1693309576/file59440.mp4?_i=AA
+// https://thecodingtrain.com/challenges/166-image-to-ascii
+// https://www.chloechoi.art/itp/week-8-exercises
