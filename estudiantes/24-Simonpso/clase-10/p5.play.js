bc633923d72c13bc237460c001606f88d77c1b85
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-05-21 21:28:38 -0400

    subir archivos correctamente

---
+++ estudiantes/24-Simonpso/clase-10/p5.play.js
@@ -0,0 +1,6 @@
+var script = document.createElement('script');
+script.src = 'https://p5play.org/v3/p5play.js';
+document.head.appendChild(script);
+console.error(
+	'The file "https://p5play.org/v3/p5.play.js" has been moved. Please load p5play from this url: "https://p5play.org/v3/p5play.js"'
+);
