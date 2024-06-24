427cd0bdd8dd8ced1820f1cc94a119272f7e8847
isabellagutierrezm <163045412+isabellagutierrezm@users.noreply.github.com>
2024-06-24 16:54:38 -0400

    Add files via upload

---
+++ estudiantes/12-isabellagutierrezm/clase-15/particle.js
@@ -0,0 +1,62 @@
+class Particle {
+  // en este proceso establecemos la velocidad de los vectores con respecto a su actualización, bordes y como se muestra a la pantalla 
+  constructor() {
+    this.pos = createVector(random(width), random(height));
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.maxspeed = 4;
+    this.prevPos = this.pos.copy();
+  }
+// establecemos actualización 
+  update() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxspeed);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+// movimiento de vectores según x, y
+  follow(vectors) {
+    var x = floor(this.pos.x / scl);
+    var y = floor(this.pos.y / scl);
+    var index = x + y * cols;
+    var force = vectors[index];
+    this.applyForce(force);
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+// establecemos el peso de la línea según las coordenadas x, y y los efectos para establecer color 
+  show() {
+    stroke(hu,sat,brt,alph);
+    strokeWeight(partStroke);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+    this.updatePrev();
+  }
+// previsualizamos las partículas segun la actualización del archivo
+  updatePrev() {
+    this.prevPos.x = this.pos.x;
+    this.prevPos.y = this.pos.y;
+  }
+
+  edges() {
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+      this.updatePrev();
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+      this.updatePrev();
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+      this.updatePrev();
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+      this.updatePrev();
+    }
+
+  }
+
+}
